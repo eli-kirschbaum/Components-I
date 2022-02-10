6a774fa3a8b7797c4c66d4d59a2607bffad9bf91
eli-kirschbaum <eli.kirschbaum1@gmail.com>
2022-02-10 02:18:27 -0600

    Completed steps 1 through 3 -- made the articleMaker component with html elements

--- src/index.js
+++ src/index.js
@@ -2,4 +2,36 @@ import './index.less'
 import './components/article/article.js'
 import './components/menu/menu.js'
 
-// initial push
+function articleMaker(articleObj) {
+    const article = document.createElement('div');
+    const articleTitle = document.createElement('h2');
+    const articleDate = document.createElement('p');
+    const pOne = document.createElement('p');
+    const pTwo = document.createElement('p');
+    const pThree = document.createElement('p');
+    const expandButton = document.createElement('span');
+    
+    article.appendChild(articleTitle);
+    article.apppendChild(articleDate);
+    article.apppendChild(pOne);
+    article.apppendChild(pTwo);
+    article.apppendChild(pThree);
+    article.apppendChild(expandButton);
+    
+    article.classList.add('article');
+    articleDate.classList.add('date');
+    expandButton.classList.add('expandButton');
+
+    articleTitle.textContent = articleObj.title;
+    articleDate.textContent = articleObj.date;
+    pOne.textContent = articleObj.firstParagraph;
+    pTwo.textContent = articleObj.secondParagraph;
+    pThree.textContent = articleObj.thirdParagraph;
+    expandButton.textContent = '+';
+
+    expandButton.addEventListener('click', event => {
+        article.classList.toggle('article-open');
+    });
+    
+    return article;
+}
